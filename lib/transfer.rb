7f817e9fbe6207a7359aea53a9683b6c27eae994
Serena Berry <siberry226@gmail.com>
2019-03-28 13:45:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
-  attr_reader :sender, :receiver
+  attr_reader :sender, :receiver, :amount
   attr_accessor :status
   
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @amount = amount
   end
 end
