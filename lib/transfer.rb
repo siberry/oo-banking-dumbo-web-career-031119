26a2f5e8e3d0ae8b999ad383bb7f7e1858e3ef96
Serena Berry <siberry226@gmail.com>
2019-03-28 13:50:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,8 +14,10 @@ class Transfer
   end
   
   def execute_transaction
-    sender.balance -= @amount
-    receiver.balance += @amount
-    self.status = "complete"
+    if self.status == "pending"
+      sender.balance -= @amount
+      receiver.balance += @amount
+      self.status = "complete"
+    end
   end
 end
